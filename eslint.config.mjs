3f80a6c15588cfe45856fc45f6dc526637c89b83
amadeussalz <168459245+amadeussalz@users.noreply.github.com>
2026-03-13 11:28:36 +0300

    module types

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,6 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
 ]);
